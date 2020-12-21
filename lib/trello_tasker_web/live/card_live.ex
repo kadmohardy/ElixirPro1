ce0b277eed31fe1b5731f5cc144aa1b272f73111
Kadmo <kadmohardy@gmail.com>
2020-12-20 23:29:55 -0300

    style page1

--- lib/trello_tasker_web/live/card_live.ex
+++ lib/trello_tasker_web/live/card_live.ex
@@ -1,8 +1,16 @@
 defmodule TrelloTaskerWeb.PageLive do
   use TrelloTaskerWeb, :live_view
 
+  alias Phoenix.View
+  alias TrelloTaskerWeb.CardView
+
   @impl true
   def mount(_params, _session, socket) do
     {:ok, socket}
+  end
+
+  @impl true
+  def render(assigns) do
+    View.render(CardView, "index.html", assigns)
   end
 end
